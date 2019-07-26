0d8d38be98f47b62eb63a89f240e21cf9ad0a19f
Bacem Ghali <ghali.bacem@gmail.com>
2019-07-26 19:54:30 +0300

    Style the navbar

--- src/main/webApp/src/Navbar.js
+++ src/main/webApp/src/Navbar.js
@@ -1,11 +1,11 @@
 
 import React from 'react'
 import classNames from 'classnames'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className={classNames('navbar', 'navbar-expand-lg', 'navbar-light', 'bg-light')}>
+        <nav id="myNavBar" className={classNames('navbar', 'navbar-expand-lg', 'navbar-light', 'bg-light')}>
             <button className={classNames('navbar-toggler')} type="button" data-toggle="collapse"
                 data-target="#myNavbar" aria-controls="myNavbar"
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -13,9 +13,9 @@ const Navbar = () => {
             </button>
             <div className={classNames('collapse', 'navbar-collapse')} id="myNavbar">
                 <div className={classNames('navbar-nav', 'navbar-center')} >
-                    <Link to="/customers"><a className={classNames('nav-item', 'nav-link')}>Customers</a></Link>
-                    <Link to="/orders"><a className={classNames('nav-item', 'nav-link')}>Orders</a></Link>
-                    <Link to="/products"><a className={classNames('nav-item', 'nav-link')}>Products</a></Link>
+                    <NavLink to="/customers" activeClassName='is-active'><a className={classNames('nav-item', 'nav-link')}>Customers</a></NavLink>
+                    <NavLink to="/orders" activeClassName='is-active'><a className={classNames('nav-item', 'nav-link')}>Orders</a></NavLink>
+                    <NavLink to="/products" activeClassName='is-active'><a className={classNames('nav-item', 'nav-link')}>Products</a></NavLink>
                 </div>
             </div>
         </nav>
